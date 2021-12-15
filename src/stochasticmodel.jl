d6d94c26943c8e9673578810b60626dfc8193498
MatteoMariani <matteomar.uni@gmail.com>
2021-12-15 16:26:05 +0000

    introduced the stochastic model struct

--- src/stochasticmodel.jl
+++ src/stochasticmodel.jl
@@ -2,7 +2,7 @@ using SparseArrays
 
 export StochasticModel, IndividualSEIR, IndividualSI, GenericStaticSM, GenericDynamicSM
 
-abstract type StochasticModel end
+abstract type AbstractStochasticModel end
 
 
 ### some specific models follow
@@ -36,46 +36,26 @@ IndividualSEIR{Rauto, Rinf, Rlat, Rrec}(θi, rout) where {Rauto, Rinf, Rlat, Rre
 Rlat(θi[2+nparams(Rauto)+nparams(Rinf):1+nparams(Rauto)+nparams(Rinf)+nparams(Rlat)]...), 
 Rrec(θi[2+nparams(Rauto)+nparams(Rinf)+nparams(Rlat):1+nparams(Rauto)+nparams(Rinf)+nparams(Rlat)+nparams(Rrec)]...))
 
-# GenericStaticSM: here out infection is common to all individuals, no per link infection
-
-struct GenericStaticSM{I,Rout} <: StochasticModel
-    T::Float64  #epidemic time
-    θ::Matrix{Float64}  #parameters
-    Λ::SparseMatrixCSC{Bool,Int}  #contact graph
-    out::Rout  #outgoing infection
-end
 
 
-
-GenericStaticSM{I}(T,θ,Λ,out::Rout) where {I,Rout} = GenericStaticSM{I,Rout}(T,θ,Λ,out)
-
-individual(M::GenericStaticSM{I}, θi) where I = I(θi, M.out)
-individual(M::GenericStaticSM, i::Int) = individual(M, @view M.θ[:,i])
-in_neighbors(M::GenericStaticSM, i::Int) = ((M.Λ.rowval[k], UnitRate()) for k ∈ nzrange(M.Λ,i))
-out_neighbors(M::GenericStaticSM, i::Int) = ((M.Λ.rowval[k], UnitRate()) for k ∈ nzrange(M.Λ,i))
-
-
-# GenericDynamicSM: similar but with per link infection rates (typically just masks)
-
-struct GenericDynamicSM{I,Rout,VR} <: StochasticModel
+struct StochasticModel{I,Rout,VR} <: AbstractStochasticModel
     T::Float64
     θ::Matrix
-    Λ::SparseMatrixCSC{Bool, Int}  #contact graph
-    Λ2::SparseMatrixCSC{Int, Int} #transposed contact graph
+    Λ::SparseMatrixCSC{Bool, Int}  
+    Λ2::SparseMatrixCSC{Int, Int}  
     out::Rout
     V::VR
 end
 
-#GenericDynamicSM{I}(T,θ,Λ,out::Rout,V::VR) where {I,Rout,VR} = GenericDynamicSM{I,Rout,VR}(T,θ,Λ,(sparse(Λ')),out,V)
 
-
-function GenericDynamicSM{I}(T,θ,G,out::Rout,V::VR) where {I,Rout,VR}
+function StochasticModel{I}(T,θ,G,out::Rout; V::VR = fill(UnitRate(), nnz(G))) where {I,Rout,VR}
     G1=SparseMatrixCSC(G.m, G.n, G.colptr, G.rowval, collect(1:nnz(G)))
     G2=sparse(G1')
-    GenericDynamicSM{I,Rout,VR}(T,θ,G,G2,out,V)
+    StochasticModel{I,Rout,VR}(T,θ,G,G2,out,V)
 end
 
-individual(M::GenericDynamicSM{I}, θi) where I = I(θi, M.out)
-individual(M::GenericDynamicSM, i::Int) = individual(M, @view M.θ[:,i])
-in_neighbors(M::GenericDynamicSM, i::Int) = ((M.Λ.rowval[k], M.V[k]) for k ∈ nzrange(M.Λ,i))
-out_neighbors(M::GenericDynamicSM, i::Int) = ((M.Λ2.rowval[k], M.V[M.Λ2.nzval[k]]) for k ∈ nzrange(M.Λ2,i))
+
+individual(M::StochasticModel{I}, θi) where I = I(θi, M.out)
+individual(M::StochasticModel, i::Int) = individual(M, @view M.θ[:,i])
+in_neighbors(M::StochasticModel, i::Int) = ((M.Λ.rowval[k], M.V[k]) for k ∈ nzrange(M.Λ,i))
+out_neighbors(M::StochasticModel, i::Int) = ((M.Λ2.rowval[k], M.V[M.Λ2.nzval[k]]) for k ∈ nzrange(M.Λ2,i))
