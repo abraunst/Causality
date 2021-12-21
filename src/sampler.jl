50260691a9829e2fc8bce3b8264bc494f3d678dd
MatteoMariani <matteomar.uni@gmail.com>
2021-12-21 13:50:19 +0000

    fixed sampler

--- src/sampler.jl
+++ src/sampler.jl
@@ -53,13 +53,14 @@ function Sampler(M::StochasticModel{<:IndividualSEIR})  #0=S  1=E  2=I  3=R
             if s[i] == 1
                 Q[i] = min(M.T, delay(individual(M,i).latency,zero(M.T))+t)
             else 
+                x[i,3] = min(M.T, delay(individual(M,i).recov,zero(M.T))+t)    
                 for (j,rij) ∈ out_neighbors(M,i)
                     if s[j] == 0 
-                        Q[j] = min(Q[j], delay(shift(individual(M,i).out,t) * rij * individual(M,j).inf, t))
+                        tij = delay(shift(individual(M,i).out,t) * rij * individual(M,j).inf, t)
+                        Q[j] = min( Q[j], (tij < x[i,3] ? tij : M.T) )
                     end
                 end
-                s[i] = 3
-                x[i,3] = min(M.T, delay(individual(M,i).recov,zero(M.T))+t)
+                s[i] = 3                
             end
             
         end
@@ -69,31 +70,36 @@ end
 
 
 
+compatibility(x, O, Mp::StochasticModel{<:IndividualSI}) = all((x[i,1] < t) == s for (i,s,t,p) in O)
+compatibility(x, O, Mp::StochasticModel{<:IndividualSEIR}) = all((x[i,2]<t<x[i,3]) == s for (i,s,t,p) in O)
 
 
 
-function prior(sample!; numsamples=10^5)
+
+function prior(M::StochasticModel; numsamples=10^5)
+    sample! = Sampler(M)
     N = sample!.N
-    x = zeros(N);
-    stats = zeros(N, numsamples)
+    x = zeros(N, n_states(M) - 1);
+    stats = zeros(N, n_states(M)-1, numsamples)
     @showprogress for i=1:numsamples
         sample!(x)
-        stats[:, i] .= x
+        stats[:, :, i] .= x
     end
     stats
 end
 
-function post(Mp, O; numsamples=10^4, stats = zeros(size(Mp.Λ,1), numsamples))
+function post(Mp, O; numsamples=10^4)
     sample! = Sampler(Mp)
     N = size(Mp.Λ,1)
-    x = zeros(N);
+    stats = zeros(N, n_states(Mp) - 1, numsamples)
+    x = zeros(N, n_states(Mp) -1);
     @showprogress for m=1:numsamples
         ok = false
         while !ok
             sample!(x)
-            ok = all((x[i] < t) == s for (i,s,t,p) in O)
+            ok = compatibility(x, O, Mp)
         end
-        stats[:, m] .= x
+        stats[:, :, m] .= x
     end
     stats
 end
@@ -108,6 +114,23 @@ function reweighted_post(Mp, M, O; numsamples=10^4, stats = zeros(size(Mp.Λ,1),
         sample!(x)
         weights[m] = logQ(x, Mp) + logO(x, O, Mp) - logQ(x, M)
         stats[:, m] .= x
+    end
+    weights .= exp.(weights .- minimum(weights)) 
+    weights ./= sum(weights)
+    stats, weights
+end
+
+
+function reweighted_post(Mp, M, O; numsamples=10^4, )
+    weights = zeros(numsamples)
+    sample! = Sampler(M)
+    N = size(Mp.Λ,1)
+    stats = zeros(N, n_states(M) -1, numsamples)
+    x = zeros(N, n_states(M) -1);
+    @showprogress for m=1:numsamples
+        sample!(x)
+        weights[m] = logQ(x, Mp) + logO(x, O, Mp) - logQ(x, M)
+        stats[:, :,  m] .= x
     end
     weights .= exp.(weights .- minimum(weights)) 
     weights ./= sum(weights)
