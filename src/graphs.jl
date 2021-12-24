832b9f85274c793d728b8cccedca2cbe313a6c40
Alfredo Braunstein <alfredo.braunstein@polito.it>
2021-12-24 13:26:45 +0100

    doh!

--- src/graphs.jl
+++ src/graphs.jl
@@ -5,7 +5,7 @@ makeGNP(N, rho) = sprand(N, N, rho, n->fill(true, n)) |> x->tril(x,-1) |> (x->x.
 
 function makeBarabasi(N; k=1)
     E = Graphs.edges(Graphs.barabasi_albert(N, k))
-    A = sparse([e.src for e in E], [e.dst for e in E], fill(true, length(E), N, N))
+    A = sparse([e.src for e in E], [e.dst for e in E], fill(true, length(E)), N, N)
     A .+= A'
     return SparseMatrixDiGraph(A)
 end
