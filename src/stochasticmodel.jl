1a12021dab7de5a9547e6020ddc5c5dba1282154
matteomar26 <matteomar.uni@gmail.com>
2022-01-25 13:50:32 +0000

    modification to sto_model

--- src/stochasticmodel.jl
+++ src/stochasticmodel.jl
@@ -15,7 +15,9 @@ struct IndividualSI{T,Rauto,Rinf,Rout}
     out::Rout
 end
 
-IndividualSI{Rauto, Rinf}(θi, rout) where {Rauto, Rinf} = @views IndividualSI(θi[1], Rauto(θi[2:1+nparams(Rauto)]...), Rinf(θi[2+nparams(Rauto):1+nparams(Rauto)+nparams(Rinf)]...), rout)
+IndividualSI{Rauto, Rinf, Rout}(θi, θgen) where {Rauto, Rinf, Rout} = @views IndividualSI(θi[1], Rauto(θi[2:1+nparams(Rauto)]...), Rinf(θi[2+nparams(Rauto):1+nparams(Rauto)+nparams(Rinf)]...), 
+Rout(θgen[1:nparams(Rout)])
+)
 
 
 
@@ -32,24 +34,24 @@ struct IndividualSEIR{T,Rauto,Rinf,Rout,Rlat,Rrec,Rgenlat,Rgenrec}
     recov_delay::Rgenrec
 end
 
-IndividualSEIR{Rauto, Rinf, Rlat, Rrec}(θi, (rout, rlat, rrec)) where {Rauto, Rinf, Rlat, Rrec} = @views IndividualSEIR(θi[1],
+IndividualSEIR{Rauto, Rinf, Rout, Rlat, Rgenlat, Rrec, Rgenrec}(θi, θgen) where {Rauto, Rinf, Rout, Rlat, Rgenlat, Rrec, Rgenrec} = @views IndividualSEIR(θi[1],
     Rauto(θi[2:1+nparams(Rauto)]...),
     Rinf(θi[2+nparams(Rauto):1+nparams(Rauto)+nparams(Rinf)]...),
-    rout,
+    Rout(θgen[1:nparams(Rout)]),
     Rlat(θi[2+nparams(Rauto)+nparams(Rinf):1+nparams(Rauto)+nparams(Rinf)+nparams(Rlat)]...),
-    rlat,
+    Rgenlat(θgen[nparams(Rout)+1:nparams(Rout)+nparams(Rgenlat)]),
     Rrec(θi[2+nparams(Rauto)+nparams(Rinf)+nparams(Rlat):1+nparams(Rauto)+nparams(Rinf)+nparams(Rlat)+nparams(Rrec)]...),
-    rrec)
+    Rgenrec(θgen[nparams(Rout)+nparams(Rgenlat)+1:nparams(Rout)+nparams(Rgenlat)+nparams(Rgenrec)]))
 
-struct StochasticModel{I,GT,Rgen,VR} <: AbstractStochasticModel
+struct StochasticModel{I,GT,VR} <: AbstractStochasticModel
     T::Float64
     θ::Matrix{Float64}
     G::GT
-    gen::Rgen
+    θgen::Matrix{Float64}
     V::VR
 end
 
-StochasticModel(::Type{I}, T, θ, G::GT, gen::Rgen, V::VR = fill(UnitRate(), ne(G))) where {I,GT,Rgen,VR} = StochasticModel{I,GT,Rgen,VR}(T,θ,G,gen,V)
+StochasticModel(::Type{I}, T, θ, G::GT, θgen, V::VR = fill(UnitRate(), ne(G))) where {I,GT,VR} = StochasticModel{I,GT,VR}(T,θ,G,θgen,V)
 
 individual(M::StochasticModel{I}, θi) where I = I(θi, M.gen)
 individual(M::StochasticModel, i::Int) = individual(M, @view M.θ[:,i])
