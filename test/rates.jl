f59d2cadde781ef12a35b210ad603ae6eb2e6abe
Alfredo Braunstein <alfredo.braunstein@polito.it>
2022-01-25 12:26:54 +0100

    adjust test

--- test/rates.jl
+++ test/rates.jl
@@ -1,4 +1,4 @@
-using Causality: shift, cumulated, infect
+using Causality: shift, cumulated, delay
 using IntervalUnionArithmetic
 
 @testset "Rates" begin
@@ -6,5 +6,5 @@ using IntervalUnionArithmetic
 	g = shift(GaussianRate(0.9,5.0,5.0),4.0)
 	mask = Interval(0.1,4.0)∪Interval(5.0,7.0)∪Interval(8.,15.)
 	mg = MaskedRate(g, mask)
-	@test isapprox(exp(-cumulated(mg,7.5)), sum(infect(mg,0.0)>7.5 for i=1:M)/M, atol=1/sqrt(M))
+	@test isapprox(exp(-cumulated(mg,7.5)), sum(delay(mg,0.0)>7.5 for i=1:M)/M, atol=1/sqrt(M))
 end
