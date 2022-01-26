f81429890cb70e59ff48b8f59bf310f7390c43ae
Alfredo Braunstein <alfredo.braunstein@polito.it>
2022-01-26 18:05:58 +0100

    simplify some more...

--- src/graphs.jl
+++ src/graphs.jl
@@ -1,31 +1,8 @@
-using SparseArrays, LinearAlgebra
 using IndexedGraphs
+import Graphs
 
-makeGNP(N, rho) = sprand(N, N, rho, n->fill(true, n)) |> x->tril(x,-1) |> (x->x.|=x') |> dropzeros! |> IndexedBiDiGraph
+makeGNP(N, rho) = Graphs.erdos_renyi(N, rho) |> IndexedBiDiGraph
 
-function makeBarabasi(N; k=1)
-    E = Graphs.edges(Graphs.barabasi_albert(N, k))
-    A = sparse([e.src for e in E], [e.dst for e in E], fill(true, length(E)), N, N)
-    A .+= A'
-    return IndexedBiDiGraph(A)
-end
+makeBarabasi(N; k=1) = Graphs.barabasi_albert(N, k) |> IndexedBiDiGraph
 
-
-function makeProximity(N,R2)
-    lattice = rand(N,2)
-    edgesL = []
-    edgesR = []
-    for i=1:N
-        for j=1:i-1
-            if (lattice[i,1]-lattice[j,1])^2+(lattice[i,2]-lattice[j,2])^2 < R2 
-                append!(edgesL,i)
-                append!(edgesR,j)
-            end
-        end
-    end
-    A = sparse(edgesL, edgesR, fill(true, length(edgesL)), N, N)
-    A .+= A'
-    return IndexedBiDiGraph(A)
-end
-
-
+makeProximity(N, R2) = Graphs.euclidean_graph(N, 2; seed=-1, L=1., p=2., cutoff=sqrt(R2), bc=:open) |> first |> IndexedBiDiGraph
