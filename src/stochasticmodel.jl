ceee078905a067e6a8c847979d477fe5e17dd478
Alfredo Braunstein <alfredo.braunstein@polito.it>
2022-01-27 19:00:20 +0100

    another fix

--- src/stochasticmodel.jl
+++ src/stochasticmodel.jl
@@ -17,6 +17,6 @@ end
 StochasticModel(::Type{I}, T, θ, G::GT, θgen, V::VT = fill(UnitRate(), ne(G))) where {I,GT,VT} = StochasticModel{I,GT,VT}(T,θ,G,θgen,V)
 
 individual(M::StochasticModel{I}, θi) where {I} = individual(I, θi, M.θgen)
-individual(M::StochasticModel{I}, i::Int, θgen = @view M.θ[:,i]) where {I} = individual(I, (@view M.θ[:,i]), θgen)
+individual(M::StochasticModel{I}, i::Int, θgen = M.θgen) where {I} = individual(I, (@view M.θ[:,i]), θgen)
 in_neighbors(M::StochasticModel, i::Int) = ((e.src, M.V[e.idx]) for e ∈ inedges(M.G, i))
 out_neighbors(M::StochasticModel, i::Int) = ((e.dst, M.V[e.idx]) for e ∈ outedges(M.G, i))
