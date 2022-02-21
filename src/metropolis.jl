6c0e13f8dc7b9b0f3168e4b292936703764a4689
Giovanni Catania <giovanni.catania@polito.it>
2022-02-21 15:49:16 +0100

    fix metropolis

--- src/metropolis.jl
+++ src/metropolis.jl
@@ -7,52 +7,20 @@ struct UniformMove{T<:AbstractFloat}
 end
 
 struct GaussMove{T<:AbstractFloat}
-    σ::T
+    δ::T
 end
 
-function propose_move(K::UniformMove,x,T)
-    a = max(0,x-K.δ)
-    rand()*(min(x+K.δ,T)-a)+a
+function propose_move!(K::UniformMove,x,xnew,i,Mp)
+    a = max(0,x[i]-K.δ)
+    xnew[i] = rand()*(min(x[i]+K.δ,Mp.T)-a)+a
 end
 
-function logdensity(K::UniformMove,μ,x,T)
-    -log((min(μ+K.δ,T)-max(0,μ-K.δ)))
+function propose_move!(K::GaussMove,x,xnew,i,Mp)
+    xnew[i] = rand(TruncatedNormal(x[i],K.δ^2,0.0,Mp.T))
 end
 
-function propose_move(K::GaussMove,x,T)
-    rand(TruncatedNormal(x,K.σ,0.0,T))
-end
-
-function logdensity(K::GaussMove,μ,x,T)
-    logpdf(TruncatedNormal(μ,K.σ,0.0,T),x)
-end
-
-
-function metropolis_hasting(Mp, O, K; numsteps=10^3,nfirst = numsteps,x = post(Mp,O,numsamples = 1)[:])
-    
-    N = nv(Mp.G)
-    T = Mp.T
-    xnew = similar(x)
-    #stats = copy(x)
-    stats = zeros(N,numsteps)
-    perm = collect(1:N)
-
-    @showprogress for m=1:(numsteps+nfirst)
-        
-        randperm!(perm)
-        xnew = copy(x)
-        for i in perm
-            xnew[i] = propose_move(K,x[i],T)
-            if rand() < exp(-ΔE(x,xnew,i,Mp,O) + logdensity(K,xnew[i],x[i],T) - logdensity(K,x[i],xnew[i],T) )
-                x = copy(xnew)
-            end
-        end
-        if m > nfirst
-            stats[:,m-nfirst] = xnew
-        end
-    end
-    stats
-end
+logdensity(K::UniformMove,μ,x,T) = -log((min(μ+K.δ,T)-max(0,μ-K.δ)))
+logdensity(K::GaussMove,μ,x,T) = logpdf(TruncatedNormal(μ,K.δ^2,0.0,T),x)
 
 function ΔE(x,xnew,i,Mp,O)
     s = (logQi(Mp,i,individual(Mp,i),x) + logO(x, O, Mp)) - (logO(xnew, O, Mp) + logQi(Mp,i,individual(Mp,i),xnew))
@@ -60,6 +28,70 @@ function ΔE(x,xnew,i,Mp,O)
         s += logQi(Mp,j,individual(Mp,j),x) - logQi(Mp,j,individual(Mp,j),xnew)
     end
     s
+end
+
+function metropolis_hasting_mc(Mp, O, K; numsteps=10^3,x = post(Mp,O,numsamples = 1)[:])
+    
+    N = nv(Mp.G)
+    T = Mp.T
+    xnew = similar(x)
+    acc_ratio = 0.0
+    @showprogress for m=1:numsteps
+
+        xnew = copy(x)
+        i = rand(1:N)
+        propose_move!(K,x,xnew,i,Mp)
+        if rand() < exp(-ΔE(x,xnew,i,Mp,O) + logdensity(K,xnew[i],x[i],T) - logdensity(K,x[i],xnew[i],T) )
+            x = copy(xnew)
+            acc_ratio+=1
+        end
+    end
+    xnew, acc_ratio/numsteps
+end
+
+function metropolis_sampling_parallel(Mp, O, K; numsamples = 10^3,numsteps=10^3)
+
+    N = nv(Mp.G)
+    nt = Threads.nthreads()
+    stats = [Vector{Float64}(undef,0) for t=1:nt]
+
+    pr = Progress(numsamples)
+
+    #ProgressMeter.update!(pr,0)
+    #jj = Threads.Atomic{Int}(0)
+    #l = Threads.SpinLock()
+
+    Threads.@threads for m = 1:numsamples
+        ti = Threads.threadid()
+        append!(stats[ti],metropolis_hasting_mc(Mp,O,K;numsteps = numsteps)[1])
+        
+        #Threads.atomic_add!(jj, 1)
+        #Threads.lock(l)#acc_vec = zeros(length(δvec),numsamples)
+        #ProgressMeter.update!(pr, jj[])
+        #Threads.unlock(l) 
+    end
+    
+    return collect(reshape(vcat(stats...),(N,numsamples))')
+end
+
+
+function metropolis_sampling_sequential(Mp, O, K; numsamples = 10^3,numsteps=10^3,nfirst = 10^3, x = post(Mp,O,numsamples = 1)[:])
+
+    n_states(Mp)!=2 && @error "only working for SI "
+    
+    N = nv(Mp.G)
+    stats = zeros(numsamples,N)
+    pr = Progress(numsamples)
+
+    x,~ = metropolis_hasting_mc(Mp,O,K;numsteps = numsteps,x = x)
+
+    for m = 1:numsamples
+        x, acc_ratio = metropolis_hasting_mc(Mp,O,K;numsteps = numsteps,x = x)
+        stats[m,:]  = x
+        ProgressMeter.next!(pr,showvalues=[(:acc_ratio,acc_ratio)] )
+    end
+    
+    stats
 end
 
 
